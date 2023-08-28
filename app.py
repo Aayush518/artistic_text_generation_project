25d1088b65956d5ce45f47816f097c4c1515beff
Aayush Adhikari <adhikariaayush37@gmail.com>
2023-08-28 15:30:47 +0545

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from models.lstm_model import train_lstm_model
 from models.generator import generate_text
 from tensorflow.keras.preprocessing.text import Tokenizer
+import random
 
 def main():
     st.title('Artistic Text Generation')
@@ -15,7 +16,7 @@ def main():
 
     seed_text = st.text_area("Enter your seed text here")
 
-    # Additional features: text style and word count
+    # Additional features: text style, word count, and style suggestions
     selected_style = st.selectbox("Select Text Style", ["Elegant", "Playful", "Mysterious"])
     word_count_limit = st.number_input("Word Count Limit", min_value=10, max_value=500, value=100)
 
@@ -26,6 +27,14 @@ def main():
             generated_text = generate_text(model, tokenizer, seed_text, num_words=word_count_limit)
             st.markdown("## Generated Artistic Text")
             st.write(generated_text, unsafe_allow_html=True)
+
+            # Generate style suggestions based on the selected style
+            style_suggestions = []
+            if selected_style in styles:
+                style_suggestions = random.sample(styles[selected_style], 3)
+            st.markdown("### Style Suggestions:")
+            for suggestion in style_suggestions:
+                st.write(suggestion)
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template, request
 from models.lstm_model import train_lstm_model
 from models.generator import generate_text
 from tensorflow.keras.preprocessing.text import Tokenizer
+import random
 
 app = Flask(__name__)
 
 # Global variables to store user settings
 selected_style = None
 word_count_limit = 100
+
+# Additional styles for text generation
+styles = {
+    "Elegant": ["beautiful", "graceful", "refined"],
+    "Playful": ["fun", "joyful", "lively"],
+    "Mysterious": ["enigmatic", "intriguing", "cryptic"]
+}
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -21,15 +29,27 @@ def index():
     model = train_lstm_model(artistic_texts)
 
     generated_text = ""
+    style_suggestions = []
 
     if request.method == 'POST':
         seed_text = request.form['seed_text']
+
+        # Handle the 'style' field
+        selected_style = request.form.get('style')
+        if selected_style and selected_style in styles:
+            style_suggestions = random.sample(styles[selected_style], 3)
+
+        # Handle the 'word_count' field
+        word_count_limit = request.form.get('word_count', 100)
+        if word_count_limit:
+            word_count_limit = int(word_count_limit)
+
         if seed_text:
             tokenizer = Tokenizer()
             tokenizer.fit_on_texts(artistic_texts)
             generated_text = generate_text(model, tokenizer, seed_text, num_words=word_count_limit)
 
-    return render_template('index.html', generated_text=generated_text)
+    return render_template('index.html', generated_text=generated_text, style_suggestions=style_suggestions)
 
 if __name__ == "__main__":
     app.run(debug=True)
